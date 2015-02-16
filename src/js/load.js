7efa683f468469775827ed0b6af69a4e148d044c
Dewitt Buckingham <dj729@comcast.net>
2015-02-16 01:57:26 -0800

    Configure automation (Grunt)
    * Add Grunt
    * Configure build tasks
    * Build foundation templates

---
+++ src/js/load.js
@@ -0,0 +1,4 @@
+/* HTML5 feature test*/
+if( 'querySelector' in document && 'addEventListener' in window && 'localStorage' in window ) {
+    // Load scripts here
+}
---
+++ src/php/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Zero Daedalus</title>
+</head>
+<body>
+    
+</body>
+</html>
